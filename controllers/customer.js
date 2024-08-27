4be64f9074c484603a4881e5901dab17a6dbd211
dinelka <newlakshan3@gmail.com>
2024-08-27 23:24:42 +0530

    update id

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
+  // Optionally, generate a new customer ID when the page loads
+  window.onload = function () {
+    generateNewCustomerId();
+  };
+
   $("#save-customer").click(function () {
     event.preventDefault();
 
-    let customerIdF = $("#customerId").val();
-    let customerNameF = $("#customerName").val();
-    let customerPhoneF = $("#customerPhone").val();
-    let customerAddressF = $("#customerAddress").val();
+    let id = $("#customerId").val();
+    let name = $("#customerName").val();
+    let phone = $("#customerPhone").val();
+    let address = $("#customerAddress").val();
 
     const customerData = {
-      customerid: customerIdF,
-      customername: customerNameF,
-      customerphone: customerPhoneF,
-      customeraddress: customerAddressF,
+      id: id,
+      name: name,
+      phone: phone,
+      address: address,
     };
     console.log(customerData);
 
@@ -19,16 +24,41 @@ $(document).ready(function () {
     console.log(customerJSON);
 
     $.ajax({
-      url: "http://localhost:8080/CafeManangement2024/customer",
+      url: "http://localhost:8080/CafeManagement2024/customer",
       type: "POST",
       data: customerJSON,
       headers: { "Content-Type": "application/json" },
       success: (res) => {
         console.log(JSON.stringify(res));
+
+        generateNewCustomerId();
+        clearCustomerFields();
       },
       error: (res) => {
         console.error(res);
       },
     });
   });
+
+  // Event listener for the "Add" button
+
+  function generateNewCustomerId() {
+    $.ajax({
+      url: "http://localhost:8080/CafeManagement2024/customer", // URL of your servlet
+      method: "GET",
+      success: (res) => {
+        // Populate the Customer ID input field with the response data
+        $("#customerId").val(res);
+      },
+      error: function (xhr, status, error) {
+        console.error("Error:", error);
+      },
+    });
+  }
+  function clearCustomerFields() {
+    $("#customerId").val("");
+    $("#customerName").val("");
+    $("#customerPhone").val("");
+    $("#customerAddress").val("");
+  }
 });
